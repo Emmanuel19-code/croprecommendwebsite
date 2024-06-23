59c222c672c5a008df8795e1ab998824572a619a
EMMANUEL <83466358+Emmanuel19-code@users.noreply.github.com>
2024-06-23 15:30:40 +0000

    new update

--- app.py
+++ app.py
@@ -1,32 +1,31 @@
-from flask import Flask, jsonify,render_template,request
-#the render_template is used to render templates in flask
+from flask import Flask, jsonify, render_template, request
 import pickle
 
 app = Flask(__name__)
 
 crops = {
-    1:'Rice', 
-    2:'Maize', 
-    3:'ChickPea', 
-    4:'KidneyBeans', 
-    5:'PigeonPeas',
-    6:'MothBeans', 
-    7:'MungBean', 
-    8:'Blackgram', 
-    9:'Lentil', 
-    10:'Pomegranate',
-    11:'Banana', 
-    12:'Mango', 
-    13:'Grapes', 
-    14:'Watermelon', 
-    15:'Muskmelon', 
-    16:'Apple',
-    17:'Orange', 
-    18:'Papaya', 
-    19:'Coconut', 
-    20:'Cotton', 
-    21:'Jute', 
-    22:'Coffee'
+    1: 'Rice',
+    2: 'Maize',
+    3: 'ChickPea',
+    4: 'KidneyBeans',
+    5: 'PigeonPeas',
+    6: 'MothBeans',
+    7: 'MungBean',
+    8: 'Blackgram',
+    9: 'Lentil',
+    10: 'Pomegranate',
+    11: 'Banana',
+    12: 'Mango',
+    13: 'Grapes',
+    14: 'Watermelon',
+    15: 'Muskmelon',
+    16: 'Apple',
+    17: 'Orange',
+    18: 'Papaya',
+    19: 'Coconut',
+    20: 'Cotton',
+    21: 'Jute',
+    22: 'Coffee'
 }
 
 @app.route("/")
@@ -35,10 +34,9 @@ def home():
 
 @app.route("/me")
 def me():
-    return "heloo this is me"
+    return "Hello, this is me"
 
-
-@app.route("/recommend",methods=["POST"])
+@app.route("/recommend", methods=["POST"])
 def recommend():
     try:
         nitrogen = float(request.form.get("nitrogen"))
@@ -48,15 +46,18 @@ def recommend():
         temperature = float(request.form.get("temperature"))
         humidity = float(request.form.get("humidity"))
         ph_value = float(request.form.get("ph_value"))
-        with open("model_one.pkl","rb") as f:
+        
+        with open("model_one.pkl", "rb") as f:
             model = pickle.load(f)
-        result =  model.predict([[nitrogen,phosphorus,potassium,temperature,humidity,ph_value,rainfall]])
+        
+        result = model.predict([[nitrogen, phosphorus, potassium, temperature, humidity, ph_value, rainfall]])
         recommended_crop = crops[result[0]]
-        return render_template("home.html",crop=recommended_crop)
-    except:
-        return "An error occured",404
-    
+        
+        return render_template("home.html", crop=recommended_crop)
+    except ValueError:
+        return "Invalid input. Please enter valid numbers.", 400
+    except Exception as e:
+        return f"An error occurred: {e}", 500
 
 if __name__ == "__main__":
     app.run(debug=True)
-
